11651b1058f2384ed441b0c97d49212739f6c400
Emmanuel <emmanuelmartinez122623@gmail.com>
2022-11-02 17:05:22 -0700

    Add Mock article interface

---
+++ FrontEnd/newsies-app/src/models/MockArticle.ts
@@ -0,0 +1,11 @@
+export interface MockArticle {
+    id : number, 
+    author: string,
+    title: string,
+    description: string,
+    url: string,
+    urlToImage : string,
+    sentiment : number 
+    publishedAt : string
+
+}
